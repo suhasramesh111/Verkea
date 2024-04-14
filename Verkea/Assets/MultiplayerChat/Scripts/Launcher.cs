9b4a3a26979980c95d171d652e8578dd2771c4af
Ananyagopalakrishnaa <ananyagopalakrishnaa@gmail.com>
2024-04-14 04:48:19 -0500

    fully functioning voice chat with scene transitions

--- Verkea/Assets/MultiplayerChat/Scripts/Launcher.cs
+++ Verkea/Assets/MultiplayerChat/Scripts/Launcher.cs
@@ -2,74 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-using TMPro;
 using Photon.Realtime;
+using Photon.Voice.PUN;
+using Photon.Voice.Unity;
+using TMPro;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
     public static Launcher Instance;
-
 
     [SerializeField] TMP_InputField roomNameInputField;
     [SerializeField] TMP_Text errorText;
     [SerializeField] TMP_Text roomNameText;
     [SerializeField] Transform roomListContent;
     [SerializeField] GameObject roomListItemPrefab;
+    [SerializeField] LoadingMenuScript roomMenu;
 
-    public LoadingMenuScript roomMenu;
-
+    public GameObject voiceManager;
 
     void Awake()
     {
         Instance = this;
-
     }
-    // Start is called before the first frame update
+
     void Start()
     {
-        Debug.Log("joined a Master");
-        PhotonNetwork.ConnectUsingSettings(); 
+        PhotonNetwork.ConnectUsingSettings();
     }
 
     public override void OnConnectedToMaster()
     {
         PhotonNetwork.JoinLobby();
-        //PhotonNetwork.AutomaticallySyncScene = true;
     }
 
     public override void OnJoinedLobby()
     {
-        Debug.Log("joined a lobby");
-        if (MenuManager.Instance == null)
-        {
-            Debug.Log("instance is null not correct");
-        }
-        
-        else
-        {
-            MenuManager.Instance.OpenMenu("Title");
-        }
-        
-
+        MenuManager.Instance.OpenMenu("Title");
     }
 
     public void CreateRoom()
     {
-        Debug.Log("create button clicked in CREATE ROOM");
         if (string.IsNullOrEmpty(roomNameInputField.text))
         {
-            return; 
+            return;
         }
         PhotonNetwork.CreateRoom(roomNameInputField.text);
         MenuManager.Instance.OpenMenu("Loading");
-
     }
 
     public override void OnJoinedRoom()
     {
-        Debug.Log("room joined ");
         MenuManager.Instance.OpenMenu("room menu");
         roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+
+        VoiceManager.Instance.StartVoiceChat();
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
@@ -84,35 +70,24 @@ public class Launcher : MonoBehaviourPunCallbacks
         MenuManager.Instance.OpenMenu("Loading");
     }
 
-
     public void JoinRoom(RoomInfo info)
     {
-        Debug.Log("join room clicked");
         if (info.Name != null)
         {
-            Debug.Log("the room name is " + info.Name);
             PhotonNetwork.JoinRoom(info.Name);
         }
-        else
-        {
-            Debug.Log("no room found");
-        }
-        
         MenuManager.Instance.OpenMenu("Loading");
     }
-
-
 
     public override void OnLeftRoom()
     {
         MenuManager.Instance.OpenMenu("Title");
+        VoiceManager.Instance.StopVoiceChat(); // Stop voice chat when leaving the room
     }
-
-    
 
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
-        foreach( Transform trans in roomListContent)
+        foreach (Transform trans in roomListContent)
         {
             Destroy(trans.gameObject);
         }
@@ -123,20 +98,20 @@ public class Launcher : MonoBehaviourPunCallbacks
         }
     }
 
-
     public void StoreScene()
     {
-
         MenuManager.Instance.CloseMenu(roomMenu);
-        Debug.Log("switch to store scene");
         PhotonNetwork.LoadLevel(1);
     }
 
     public void HomeScene()
     {
         MenuManager.Instance.CloseMenu(roomMenu);
-        Debug.Log("switch to home scene");
         PhotonNetwork.LoadLevel(2);
     }
 
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
 }
