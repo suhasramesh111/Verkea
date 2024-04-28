50e5b8e07d997718d3a3cc682e6c0d772bd01693
suhasramesh111 <suhas.ramesh.111@gmail.com>
2024-04-27 19:52:34 -0500

    Added trees and skybox. Made UI changes with menus. Made small correction in house object menu position. Made changes to text outputs in the game.

--- Verkea/Assets/Scripts/HomeOutline.cs
+++ Verkea/Assets/Scripts/HomeOutline.cs
@@ -76,7 +76,7 @@ public class HomeOutline : MonoBehaviourPun
 
                 if (lastHoveredObject != null)
                 {
-                    Vector3 panelPosition = lastHoveredObject.transform.position + Vector3.up * 30f;
+                    Vector3 panelPosition = lastHoveredObject.transform.position + Vector3.up * 2f;
                     ObjectMenuPanel.transform.position = panelPosition;
 
                     Vector3 directionToCamera = Camera.main.transform.position - ObjectMenuPanel.transform.position;
